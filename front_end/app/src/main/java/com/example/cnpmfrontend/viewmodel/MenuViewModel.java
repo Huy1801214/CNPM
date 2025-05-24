9c5dac02dbe39e2d4f98708a333fda1857769977
dfong <169019499+dndphong2703@users.noreply.github.com>
2025-05-24 20:45:53 +0700

    Làm chức năng thêm món

--- front_end/app/src/main/java/com/example/cnpmfrontend/viewmodel/MenuViewModel.java
+++ front_end/app/src/main/java/com/example/cnpmfrontend/viewmodel/MenuViewModel.java
@@ -3,24 +3,35 @@ package com.example.cnpmfrontend.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.cnpmfrontend.model.DrinkCategory;
 import com.example.cnpmfrontend.model.DrinkItem;
+import com.example.cnpmfrontend.model.DrinkItemRequest;
 import com.example.cnpmfrontend.repository.MenuRepository;
+
 import java.util.List;
 
 public class MenuViewModel extends ViewModel {
-    private MenuRepository menuRepository;
+    private MenuRepository menuRepository = new MenuRepository();
+
     private MutableLiveData<String> errorMessage = new MutableLiveData<>();
     private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
 
     private LiveData<List<DrinkItem>> allDrinksLiveData;
-
-    public MenuViewModel() {
-        menuRepository = new MenuRepository();
-    }
+    private LiveData<List<DrinkCategory>> allCategoriesLiveData;
 
     public LiveData<List<DrinkItem>> getAllDrinks() {
         allDrinksLiveData = menuRepository.getAllDrinks(errorMessage, isLoading);
         return allDrinksLiveData;
+    }
+
+    public LiveData<List<DrinkCategory>> getAllCategories() {
+        allCategoriesLiveData = menuRepository.getAllCategories(errorMessage, isLoading);
+        return allCategoriesLiveData;
+    }
+
+    public LiveData<String> addDrinkItem(DrinkItemRequest request) {
+        return menuRepository.addDrinkItem(request, isLoading, errorMessage);
     }
 
     public LiveData<String> getErrorMessage() {
